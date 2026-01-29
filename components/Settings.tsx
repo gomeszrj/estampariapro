c3d39b60343a49c74cfd753a3612d564f783e81b
gomeszrj <renancgomesz@gmail.com>
2026-01-29 18:40:23 -0300

    fix: remove extra div in Settings.tsx

--- components/Settings.tsx
+++ components/Settings.tsx
@@ -337,6 +337,7 @@ const Settings: React.FC = () => {
               </div>
             </div>
           </div>
+
       ) : (
         <div className="animate-in fade-in zoom-in-95 duration-300 grid grid-cols-1 lg:grid-cols-12 gap-10">
           {/* Add New User */}
@@ -436,7 +437,7 @@ const Settings: React.FC = () => {
         </div>
       )
       }
-    </div>
+    </div >
   );
 };
 
